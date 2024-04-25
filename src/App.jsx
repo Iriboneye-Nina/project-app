7dfd765c9d4c0340a8e91cc37c051dcc69c5def2
Brice Kelly <ntirukelly@gmail.com>
2024-04-25 16:35:52 +0200

    new-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Layout from "./assets/pages/Layout";
-import Home from "./assets/pages/Home";
-import About from "./assets/pages/About";
-import Contact from "./assets/pages/contact";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+// import Layout from "./assets/pages/Layout";
+// import Home from "./assets/pages/Home";
+// import About from "./assets/pages/About";
+// import Contact from "./assets/pages/contact";
+import Signin from "./assets/pages/Signin"
 
 import React from 'react'
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-         <Routes>
-         <Route path="/" element={<Layout />} >
-           <Route path="/" element={<Home />} />
-           <Route path="/about" element={<About />}/>
-           <Route path="/contact" element={<Contact />}/>
-           </Route>
-           </Routes>
-    </BrowserRouter>
+    <Signin/>
+    // <>
+    // <BrowserRouter>
+    //      <Routes>
+    //      <Route path="/" element={<Layout />} >
+    //        <Route path="/" element={<Home />} />
+    //        <Route path="/about" element={<About />}/>
+    //        <Route path="/contact" element={<Contact />}/>
+    //        </Route>
+    //        </Routes>
+    // </BrowserRouter>
  
-        </>
+    //     </>
   )
 }
 
--- src/assets/pages/Layout.jsx
+++ src/assets/pages/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+
 import Sidebar from '../../components/Sidebar'
 import { Outlet } from 'react-router-dom'
 
